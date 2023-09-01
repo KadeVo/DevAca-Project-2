4ff9048002c4f67de68a00605f2455c652a49d00
Tiffany Wang <thiffanyjun@hotmail.com>
2023-09-01 13:15:48 +1200

    apply IfAuthenticated to Zombie component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -7,8 +7,9 @@ function App() {
       <div className="app">
         <h1>Take a Bite!</h1>
         <h2>Login to see the list of Zombie out there!</h2>
+
         <Nav />
-        <Zombies />
+      
       </div>
     </>
   )
